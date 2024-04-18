63002f183026401e11dd800a8167c8a9ad5bff87
DSI\paezj <jose.alexis.paez@keyloop.com>
2024-04-18 15:17:36 +0200

    Initial project creation commit

---
+++ src/avrada_fan_controller.adb
@@ -0,0 +1,11 @@
+with AVR.UART; use AVR.UART;
+with Avrada_Rts_Config;
+
+procedure Avrada_Fan_Controller is
+begin
+   Init (Baud_19200_16MHz);
+
+   Put ("Hello world!");
+   New_Line;
+
+end Avrada_Fan_Controller;
