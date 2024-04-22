995ef34604cb2ecf0cae0e5e322b04f520b91ab1
Alexis <jose.alexis.paez.thurgood@gmail.com>
2024-04-22 20:02:56 +0200

    Added first version of temperature routine

--- src/mcp9808-i2c.adb
+++ src/mcp9808-i2c.adb
@@ -11,12 +11,18 @@ package body MCP9808.I2C is
       This.Initialized := True;
    end Initialize;
    
-   procedure Get_Temperature
-     (This   : in out MCP9808_Temperature_Sensor_I2C;
-      Temp   : out Temperature;
-      Status : out Boolean) is
-   begin
-      null;
-   end Get_Temperature;
-   
+	procedure Get_Temperature
+	  (This   : in out MCP9808_Temperature_Sensor_I2C;
+	 	Temp   : out Temperature;
+	   Status : out Boolean) is
+		Temperature_Data_Raw : aliased Unsigned_16;
+		Temperature_Data     : Ambient_Temperature_Register
+		  with Address => Temperature_Data_Raw'Address;
+	begin
+		AVR.I2C.Master.Send_And_Receive
+		  (This.I2C_Address,
+	  		Unsigned_8 (TEMP_AMBIENT),
+	  		Temperature_Data_Raw);
+	end Get_Temperature;
+	
 end MCP9808.I2C;
